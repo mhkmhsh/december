96c5625731adee664039299221b2954650cbb8a9
mhkmhsh <mahekmahesh12@gmail.com>
2023-12-08 09:29:15 -0500

    added javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+let adjective = prompt('provide an adjective')
+let noun = prompt('provide an noun')
+let verb = prompt('provide an verb')
+let adverb = prompt('provide an adverb')
+
+let story = ` Once upon a time there was a ${adjective} prince, he had a ${noun}. He loved to ${verb} every single day, he was very good at ${adverb}`
+let madLibOutputDiv = document.getElementById('madLibOutput')
+
+madLibOutputDiv.innerHTML = `<p>${story}</p>`
