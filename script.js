85534944b481db75deb7b0c1069ae6a354e40c50
mhkmhsh <mahekmahesh12@gmail.com>
2023-12-13 09:12:23 -0500

    changed background

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let verb = prompt('provide an verb')
 let adverb = prompt('provide an adverb')
 
 let story = ` Once upon a time there was a ${adjective} prince, he had a ${noun}. He loved to ${verb} every single day, he was very good at ${adverb}`
+
 let madLibOutputDiv = document.getElementById('madLibOutput')
 
 madLibOutputDiv.innerHTML = `<p>${story}</p>`
