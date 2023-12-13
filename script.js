4ecd7b09b9297931de93ef9be608a7e19f29ee41
mhkmhsh <mahekmahesh12@gmail.com>
2023-12-13 09:27:11 -0500

    save

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let adjective = prompt('provide an adjective')
-let noun = prompt('provide an noun')
-let verb = prompt('provide an verb')
+let relationship = prompt('provide a relation (ex: mother, father, friend, girlfriend)')
+let noun = prompt('provide a noun')
+let verb = prompt('provide a verb')
 let adverb = prompt('provide an adverb')
+let place = promt('provide a place')
+let main = prompt('Name our sweetmaker')
+let activity = prompt('name an activity')
 
-let story = ` Once upon a time there was a ${adjective} prince, he had a ${noun}. He loved to ${verb} every single day, he was very good at ${adverb}`
+let story = ` One day a  ${adjective} sweetmaker decided he wanted to go to ${place}, he had always wanted a ${relationship}. this man was named ${main}, his trip to ${place} was going to do ${activity}
+ ${noun}
+ He loved to ${verb} every single day, he was very good at ${adverb}`
 
 let madLibOutputDiv = document.getElementById('madLibOutput')
 
