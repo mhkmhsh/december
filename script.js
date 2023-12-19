762cff2e8409a25ea2de4b800144126e39548abb
mhkmhsh <mahekmahesh12@gmail.com>
2023-12-19 09:04:07 -0500

    made it christmas

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ let adverb = prompt('provide an adverb')
 let place = prompt('provide a place')
 let main = prompt('Name our sweetmaker')
 let activity = prompt('name an activity')
+let girl = prompt('name a girl')
+let end = prompt('name a number')
 
-let story = ` One day a  ${adjective} sweetmaker decided he wanted to go to ${place}, he had always wanted a ${relationship}. this man was named ${main}, his trip to ${place} was going to do ${activity} He loved to ${verb} every single day, he was very good at ${adverb}. The ${noun} was upset that he wanted to go on this trip `
+let story = ` One day a  ${adjective} snowman went to the ${place}, he had always wanted a ${relationship}. this man was named ${main}, his trip to ${place} was going to do ${activity} He loved to ${verb} every single day, he was very good at ${adverb}. The ${noun} was upset that he wanted to go on this trip but he went anyways. On this trip he met ${girl} , she was also a snowman. together they had ${end} babies. `
 
 let madLibOutputDiv = document.getElementById('madLibOutput')
 
